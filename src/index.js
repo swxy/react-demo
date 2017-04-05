84cf8c070c96a0f26e2d70afef00be4c673f5125
swxy <swxymile@163.com>
2017-04-05 19:38:18 +0800

    通过取hash值展示不同组件

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import {AppContainer} from 'react-hot-loader';
 // AppContainer is a necessary wrapper component for HMR
 
 import Loop from './loop';
+import Test from './test';
+
+const Comp = {Loop, Test};
 
 const render = (Component) => {
     ReactDOM.render(
@@ -19,7 +22,29 @@ const render = (Component) => {
     );
 };
 
-render(Loop);
+// get which component should render
+const comp = () => {
+    let p = window.location.hash.slice(1);
+    if (!p) {
+        console.log('not found component');
+        return false;
+    }
+    p = p[0].toUpperCase() + p.slice(1);
+    if (Comp[p]) {
+        return Comp[p];
+    }
+    else {
+        console.log('not found component');
+        return false;
+    }
+}
+
+comp() && render(comp());
+
+window.addEventListener('hashchange', (e) => {
+    console.log('hash change && update component');
+    comp() && render(comp());
+});
 
 // Hot Module Replacement API
 if (module.hot) {
---
+++ src/test/index.js
@@ -0,0 +1,14 @@
+/**
+ * @file 测试文件夹
+ * Created by swxy on 2017/4/1.
+ */
+
+import React, {Component} from 'react';
+
+export default class Test extends Component {
+    render() {
+        return (
+            <h2>Just for test</h2>
+        )
+    }
+}
